1c3d489856ce42894f3e76b74d9dce65f2edc9cf
apadanag <95967301+ckanyemba@users.noreply.github.com>
2023-08-07 16:18:08 +0000

    add get books Component

---
+++ craigkanye_euou/src/components/books/get_books.jsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+import Image from 'next/image';
+/*
+export const HomeBooks = ({ data }) => (
+    <div className="home_body">
+        {data?Map(())}
+    </div>
+)
+
+*/
